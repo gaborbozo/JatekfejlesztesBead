63b87066776f8d160be74c1c137110b322db6781
Bozó Gábor <bg.bozogabor@gmail.com>
2020-12-21 22:42:33 +0100

    First Commit

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+ 
+    void FixedUpdate () {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0);
+        transform.position += move * Time.deltaTime * speed;
+    }
+}
---
+++ Scripts/SpawnCoins.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnCoins : MonoBehaviour
+{
+    public int numberOfCoins = 15;
+    public GameObject coin;
+    public Text score;
+    public Text countdown;
+    public Text endText;
+    public float timeLeft = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numberOfCoins; i++)
+        {
+            GameObject obj = Instantiate(coin) as GameObject;
+            obj.transform.position = new Vector2(Random.Range(-12,12),Random.Range(-12,12));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Coins left:" + numberOfCoins.ToString();
+        countdown.text = "Time left: " + timeLeft.ToString("0.0");
+
+        
+        if ( timeLeft < 0 )
+        {
+            endText.text = "Lost";
+        }
+        else
+        {
+            timeLeft -= Time.deltaTime;
+        }
+
+        if(numberOfCoins == 0) {
+            endText.text = "Won";
+            timeLeft = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Coin") numberOfCoins--;
+    }
+}
